e4f7de02cea37e0fc5ac91a9c0df24f4a50da0c2
Luis Fernando <fer.matias.luis@gmail.com>
2024-03-04 17:32:22 -0600

    ya corregir su nombre

--- src/ivmatisfilesorter/gui/VentanaPrincipal.java
+++ src/ivmatisfilesorter/gui/VentanaPrincipal.java
@@ -257,7 +257,7 @@ public class VentanaPrincipal extends JFrame {
 		private void metodoAcercaDe() {
 			ImageIcon logo = new ImageIcon(getClass().getResource("/ivmatisfilesorter/imagenes/logo.jpg"));
 			String mensaje = "\n Nombre de la aplicación:\n IvMatIsFileSorter \n\n Realizado por:\n Cortez Escamilla Isaac \n Hernández "
-					+ "Pérez Ivonne Estafanía \n Matías Acevedo  Luis Fernando  \n\n Correo electrónico: \n Ivmatis_filesorter@gmail.com \n\n Derechos reservados UMAR  2024";
+					+ "Pérez Ivonne Estefanía \n Matías Acevedo  Luis Fernando  \n\n Correo electrónico: \n Ivmatis_filesorter@gmail.com \n\n Derechos reservados UMAR  2024";
 
 			JOptionPane.showMessageDialog(null, mensaje, "Acerca de... IvMatIsFileSorter",
 					JOptionPane.INFORMATION_MESSAGE, logo);
