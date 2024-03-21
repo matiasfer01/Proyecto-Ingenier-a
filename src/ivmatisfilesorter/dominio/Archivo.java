adb1ef7787f45672185d29f0e575afbf9a06d09c
ChakEscamilla <153343350+ChakEscamilla@users.noreply.github.com>
2024-03-20 23:04:12 -0600

    Agrege documentacion a archivo

--- src/ivmatisfilesorter/dominio/Archivo.java
+++ src/ivmatisfilesorter/dominio/Archivo.java
@@ -3,58 +3,106 @@ package ivmatisfilesorter.dominio;
 import ivmatisfilesorter.exception.ExceptionBaseDatosArchivo;
 
 public class Archivo {
-	private String rutaArchivo;
-	private String nombreArchivo;
-	private Etiqueta listaEtiquetasEtiqueta;
+	/** Ruta del archivo. */
+    private String rutaArchivo;
+    
+    /** Nombre del archivo. */
+    private String nombreArchivo;
+    
+    /** Lista de etiquetas asociadas al archivo. */
+    private Etiqueta listaEtiquetasEtiqueta;
 
-	public Archivo() {
+    /**
+     * Constructor por defecto de la clase Archivo.
+     */
+    public Archivo() {
 
-	}
+    }
 
-	public String getRutaArchivo() {
-		return rutaArchivo;
-	}
+    /**
+     * Método para consultar el archivo.
+     */
+    public void consultarArchivo() {
 
-	public void setRutaArchivo(String rutaArchivo) {
-		this.rutaArchivo = rutaArchivo;
-	}
+    }
 
-	public String getNombreArchivo() {
-		return nombreArchivo;
-	}
+    /**
+     * Método para modificar el archivo.
+     */
+    public void modificarArchivo() {
 
-	public void setNombreArchivo(String nombreArchivo){
-		this.nombreArchivo = nombreArchivo;
-	}
+    }
 
-	public void consultarArchivo() {
+    /**
+     * Método para eliminar el archivo.
+     */
+    public void eliminarArchivo() {
 
-	}
+    }
 
-	public void modificarArchivo() {
+    /**
+     * Método para crear el archivo.
+     */
+    public void crearArchivo() {
 
-	}
+    }
 
-	public void eliminarArchivo() {
+    /**
+     * Obtiene la ruta del archivo.
+     * @return La ruta del archivo.
+     */
+    public String getRutaArchivo() {
+        return rutaArchivo;
+    }
 
-	}
+    /**
+     * Establece la ruta del archivo.
+     * @param rutaArchivo La nueva ruta del archivo.
+     */
+    public void setRutaArchivo(String rutaArchivo) {
+        this.rutaArchivo = rutaArchivo;
+    }
 
-	public void crearArchivo() {
+    /**
+     * Obtiene el nombre del archivo.
+     * @return El nombre del archivo.
+     */
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
 
-	}
+    /**
+     * Establece el nombre del archivo.
+     * @param nombreArchivo El nuevo nombre del archivo.
+     */
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
 
-	@Override
-	public String toString() {
-		return "Archivo [rutaArchivo=" + rutaArchivo + ", nombreArchivo=" + nombreArchivo + ", etiqueta="
-				+ listaEtiquetasEtiqueta + ", proyecto=" + "]";
-	}
+    /**
+     * Obtiene la lista de etiquetas asociadas al archivo.
+     * @return La lista de etiquetas asociadas al archivo.
+     */
+    public Etiqueta getListaEtiquetas() {
+        return listaEtiquetasEtiqueta;
+    }
 
-	public Etiqueta getListaEtiquetas() {
-		return listaEtiquetasEtiqueta;
-	}
+    /**
+     * Establece la lista de etiquetas asociadas al archivo.
+     * @param listaEtiquetas La nueva lista de etiquetas asociadas al archivo.
+     */
+    public void setListaEtiquetas(Etiqueta listaEtiquetas) {
+        this.listaEtiquetasEtiqueta = listaEtiquetas;
+    }
 
-	public void setListaEtiquetas(Etiqueta listaEtiquetas) {
-		this.listaEtiquetasEtiqueta = listaEtiquetas;
-	}
+    /**
+     * Representación en cadena de caracteres de la instancia de la clase Archivo.
+     * @return Una cadena de caracteres que representa la instancia de la clase Archivo.
+     */
+    @Override
+    public String toString() {
+        return "Archivo [rutaArchivo=" + rutaArchivo + ", nombreArchivo=" + nombreArchivo + ", etiqueta="
+                + listaEtiquetasEtiqueta + ", proyecto=" + "]";
+    }
 
 }
