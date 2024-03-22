3db2a942af0deddd7f86a305b57adb7a9a67fbae
Ivonne Estefania Hernandez Perez <ivonneestefania.hernandezperez@aulavirtual.umar.mx>
2024-03-22 14:30:20 -0600

    Implemente la capa 3 utilerias

--- src/ivmatisfilesorter/utilerias/UtileriasManejoArchivos.java
+++ src/ivmatisfilesorter/utilerias/UtileriasManejoArchivos.java
@@ -1,5 +1,6 @@
 package ivmatisfilesorter.utilerias;
 
+import javax.swing.JOptionPane;
 import java.awt.Desktop;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -9,41 +10,77 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
+ * Sirve para representar los diferentes métodos de la interacción entre los
+ * diferentes métodos que se utilicen con archivos.
+ * <p>
+ * Las características de la clase <code>Etiqueta</code> son:
+ * <ul>
+ * <li><code>etiqueta</code> para el nombre de la etiqueta.</li>
+ * <li><code>listaArchivosArchivo</code> para la lista de etiquetas.</li>
+ * </ul>
+ *
+ * @version 21/03/2024
+ * 
+ * @author Isaac Cortez Escamilla
+ * @author Luis Fernando Matias Acevedo
+ * @author Ivonne Estefanía Hernández Pérez
  * 
  * 
+ *         /** Clase que proporciona métodos de utilidad para el manejo de
+ *         archivos. Permite copiar archivos, crear directorios y abrir archivos
+ *         en el sistema.
  * 
- * 
- * 
- * @version 22/03/2024
- * 
+ * @version 21/03/2024
  * @author Isaac Cortez Escamilla
  * @author Luis Fernando Matias Acevedo
  * @author Ivonne Estefanía Hernández Pérez
  */
 
+
 public class UtileriasManejoArchivos {
-	/**
-	 * 
-	 * @param rutaAbsolutaImagen
-	 * @param rutaNueva
-	 */
-	public static void crearArchivo(String rutaAbsolutaImagen, String rutaNueva) {
 
-	}
+    public static void copiarArchivo(String rutaAbsolutaImagen, String rutaNueva) throws IOException {
+        try (BufferedInputStream lector = new BufferedInputStream(new FileInputStream(new File(rutaAbsolutaImagen)));
+             BufferedOutputStream escritor = new BufferedOutputStream(new FileOutputStream(new File(rutaNueva)))) {
 
-	/**
-	 * 
-	 * @param ruta
-	 */
-	public static void crearDirectorio(String ruta) {
+            byte[] buffer = new byte[1024];
+            int bytesLeidos = lector.read(buffer);
+            while (bytesLeidos > 0) {
+                escritor.write(buffer, 0, bytesLeidos);
+                bytesLeidos = lector.read(buffer);
+            }
+            
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Ocurrió un error al crear el archivo.");
+        }
+    }
 
-	}
+    public static void crearDirectorio(String ruta) throws IOException {
+        File directorio = new File(ruta);
+        if (!directorio.exists()) {
+            try {
+                if (!directorio.mkdir()) {
+                    throw new IOException("No se pudo crear el directorio.");
+                }
+            } catch (IOException e) {
+                throw new IOException("Error al intentar crear el directorio.");
+            }
+        }
+    }
 
-	/**
-	 * 
-	 * @param nombreArchivo
-	 */
-	public void abrirArchivo(String nombreArchivo) {
-
-	}
+    public void abrirArchivo(String nombreArchivo) throws IOException {
+        File archivo = new File(nombreArchivo);
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                if (archivo.exists()) {
+                    desktop.open(archivo);
+                }
+            } else {
+                throw new IOException("El sistema no admite la apertura de archivos.");
+            }
+        } catch (IOException e) {
+            throw new IOException("Ocurrió un error al abrir el archivo.");
+        }
+    }
 }
